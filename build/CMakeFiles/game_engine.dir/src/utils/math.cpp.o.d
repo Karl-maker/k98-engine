67191d6a3323eaac2eb3e247cbd80ad8b6c16ce1
Karl-Maker <karljohanbailey98@gmail.com>
2026-04-01 13:11:24 -0400

    fix: remove build folder

--- build/CMakeFiles/game_engine.dir/src/utils/math.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/game_engine.dir/src/utils/math.cpp.o: /app/src/utils/math.cpp \
- /usr/include/stdc-predef.h /app/include/math.h
--- build/CMakeFiles/unit_tests.dir/src/utils/math.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/unit_tests.dir/src/utils/math.cpp.o: /app/src/utils/math.cpp \
- /usr/include/stdc-predef.h /app/include/math.h
